fb4fa4e5508d036dd0339420a620ca1f2fd63bec
hdy <fwangdo17@gmail.com>
2022-08-31 10:06:44 +0100

    feat: Add encapsulation however, need to brush it up

--- chapter5/encapsulation.ml
+++ chapter5/encapsulation.ml
@@ -89,4 +89,31 @@ module ListStackCachedSize = struct
     | ([], _) -> raise Empty
     | (_::stack,size) -> (stack, size - 1)
 
-    
+(* however, the moudle is not compatible with module type so, we need to use type. *)
+(* we need abstract data type. *)
+module type LIST_STACK = sig
+  type 'a stack (* this one is core*)
+  exception Empty
+  val empty : 'a stack
+  val is_empty : 'a stack -> bool
+  val push : 'a -> 'a stack -> 'a stack
+  val peek : 'a stack -> 'a
+  val pop : 'a stack -> 'a stack
+  val size : 'a stack -> int
+end
+
+module ListStackCachedSize : LIST_STACK = struct
+  type 'a stack = 'a list * int (* this one is core *)
+  exception Empty
+  let empty = ([], 0)
+  let is_empty = function ([], _) -> true | _ -> false
+  let push x (stack, size) = (x :: stack, size + 1)
+  let peek = function ([], _) -> raise Empty | (x :: _, _) -> x
+  let pop = function
+    | ([], _) -> raise Empty
+    | (_ :: stack, size) -> (stack, size - 1)
+  let size = snd
+end
+
+(* 5.4.3 pretty printing *)
+(* Recall that the toplevel uses this angle-bracket convention to indicate an unprintable value. We’ve encountered that before with functions and <fun>:*)
