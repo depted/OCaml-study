8e1b550da25da1b440b238eb94fec840a207c503
hdy <fwangdo17@gmail.com>
2022-09-01 09:10:42 +0100

    feat(cp5): Add efficient queue

---
+++ chapter5/compilation.ml
@@ -0,0 +1,174 @@
+(*
+  this part explains ml and mli
+  let's assume where we got a file called 'x'
+  then, x.ml is called the 'implementation' and x.mli is called the 'interface'   
+*)
+
+val x : int
+val f : int -> int
+
+let x = 0
+let y = 12
+let f x = x + y
+
+module Foo : sig
+  val x : int
+  val f : int -> int
+end = struct
+  let x = 0
+  let y = 12
+  let f x = x + y
+end
+
+module Foo
+  : sig (* insert contents of foo.mli here *) end
+= struct
+  (* insert contents of foo.ml here *)
+end
+
+module type Stack = sig 
+  type 'a t 
+  exception Empty
+  val empty : 'a t
+  val is_empty : 'a t -> bool 
+  val push : 'a -> 'a t -> 'a t 
+  val peek : 'a t -> 'a 
+  val pop : 'a t -> 'a t 
+  val size : 'a t -> int 
+  val to_list : 'a t -> 'a list 
+end 
+
+module ListStack : Stack = struct
+  type 'a t = 'a list
+  exception Empty
+  let empty = []
+  let is_empty = function [] -> true | _ -> false
+  let push = List.cons
+  let peek = function [] -> raise Empty | x :: _ -> x
+  let pop = function [] -> raise Empty | _ :: s -> s
+  let size = List.length
+  let to_list = Fun.id
+end
+
+module type Stack = sig 
+  type 'a t 
+  val empty : 'a t 
+  val is_empty : 'a t -> bool 
+  val push : 'a -> 'a t -> 'a t 
+  val peek : 'a t -> 'a option 
+  val pop : 'a t -> 'a t option 
+  val size : 'a t -> int 
+  val to_list : 'a t -> 'a list 
+end
+
+module ListStack : Stack = struct 
+  type 'a t = 'a list 
+  exception Empty 
+  let empty = []
+  let is_empty = function [] -> true | _ -> false 
+  let push = List.cons
+  let peek = function [] -> None | x::_ -> Some x 
+  let pop = function [] -> None | _::s -> Some s
+  let size = List.length 
+  let to_list = Fun.id 
+end
+
+(* fmap and bind in haskell *)
+(* useful functions written in infix way *)
+(* Option.map aka fmap *)
+
+(* type is 'a option -> ('a -> 'b) -> 'b option *)
+let ( >>| ) opt f =
+  match opt with
+  | None -> None
+  | Some x -> Some (f x)
+
+(* Option.bind *)
+(* type is 'a option -> ('a -> 'b option) -> 'b option *)
+let ( >>= ) opt f =
+  match opt with
+  | None -> None
+  | Some x -> f x
+
+module type Stack = sig 
+  type 'a t 
+  val empty : 'a t 
+  val is_empty : 'a t -> bool 
+  val push : 'a -> 'a t -> 'a t 
+  val peek : 'a t -> 'a 
+  val peek_opt : 'a t -> 'a option 
+  val pop : 'a t -> 'a t 
+  val pop_opt : 'a t -> 'a t option 
+  val size : 'a t -> int 
+  val to_list : 'a t -> 'a list 
+end 
+
+module ListStack : Stack = struct 
+  type 'a t = 'a list 
+  exception Empty 
+  let empty = []
+  let is_empty = function [] -> true | _ -> false 
+  let push = List.cons 
+  let peek = function [] -> raise Empty | x :: _ -> x 
+  let peek_opt = function [] -> None | x :: _ -> Some x 
+  let pop = function [] -> raise Empty | _ :: s -> s 
+  let pop_opt = function [] -> None | _ :: s -> Some s
+  let size = List.length 
+  let to_list = Fun.id 
+end
+
+(* okay, let's make queue module and it's own type *)
+module type Queue = sig 
+  type 'a t 
+  exception Empty 
+  val empty : 'a t 
+  val is_empty : 'a t -> bool
+  val enqueue : 'a -> 'a t -> 'a t
+  val front : 'a t -> 'a 
+  val dequeue : 'a t -> 'a t 
+  val size : 'a t -> int 
+  val to_list : 'a t -> 'a list 
+end 
+
+module ListQueue : Queue = struct 
+  type 'a t = 'a list 
+  exception Empty 
+  let empty = []
+  let is_empty = function [] -> true | _ -> false 
+  let enqueue x q = q @ [x]
+  let front = function [] -> raise Empty | x :: _ -> x 
+  let dequeue = function [] -> raise Empty | _ :: q -> q 
+  let size = List.length 
+  let to_list = Fun.id 
+end 
+
+(* Batched Queue: it is 'efficient' queue implementation *)
+module BatchedQueue : Queue = struct 
+  type 'a t = { o : 'a list; i : a' list}
+
+  exception Empty 
+  
+  let empty = {o = []; i = []}
+
+  let is_empty = function 
+    | {o = []} -> true 
+    | _ -> false 
+  
+  let enqueue x = function 
+    | {o = []} -> {o = [x]; i = []}
+    | {o; i} -> {o; i = x :: i}
+
+  let front = function 
+    | {o = []} -> raise Empty 
+    | {o = h :: _} -> h 
+
+  let dequeue = function 
+    | {o = []} -> raise Empty 
+    | {o = [_]; i} -> {o = List.rev i; i = []} (* the core is this part that should be used as less as possible *)
+    | {o = _ :: t; i} -> {o = t; i}
+
+  let size {o; i} = List.(length o + length i)
+
+  let to_list {o; i} = o @ List.rev i 
+end 
+
