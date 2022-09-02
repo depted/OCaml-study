e9dc29f7021806a9f74a2aaa4d971d970a957e16
hdy <fwangdo17@gmail.com>
2022-09-02 10:13:04 +0100

    feat: Add explanation about functor

---
+++ chapter5/functor.ml
@@ -0,0 +1,35 @@
+(*
+functor is map between categories. 
+Likewise, OCaml modules contain functions, and OCaml functors map from modules to modules.   
+*)
+
+(* Here’s a tiny example of a functor: *)
+module type X = sig
+  val x : int
+end
+
+module IncX (M : X) = struct 
+  let x = M.x = 1 
+end
+
+(* The functor name is IncX and the parameter is M
+parameterized structure   
+*)
+
+(*
+  syntax of functor:
+
+  module F (M : S) = ...
+  end   
+
+  like function, functor also has synonyn like,
+
+  module F (M : S) = ...
+  <=>
+  module F = functor (M : S) -> ...
+*)
+
+(*
+Functor types are an example of an advanced programming language feature called dependent types, 
+with which the type of an output is determined by the value of an input. 
+*)
--- chapter5/includes.ml
+++ chapter5/includes.ml
@@ -1,4 +1,5 @@
 (*
+  val mem : 'a -> 'a t -> bool 
 include is used for principled copy-and-paste   
 *)
 
@@ -27,3 +28,58 @@ module ListSetExtended = struct
   let of_list lst = List.fold_right add lst empty 
 end
 (* "include" includes all definitions of target module *)
+
+module type SetExtended = sig 
+  type 'a t 
+  val empty : 'a t 
+  val mem : 'a -> 'a t -> bool 
+  val add : 'a -> 'a t -> 'a t
+  val elements : 'a t -> 'a list 
+  val of_list : 'a list -> 'a t 
+end 
+
+module ListSetImpl = struct
+  type 'a t = 'a list
+  let empty = []
+  let mem = List.mem
+  let add = List.cons
+  let elements s = List.sort_uniq Stdlib.compare s
+end
+
+module ListSet : Set = ListSetImpl
+
+module type SetExtended = sig
+  include Set
+  val of_list : 'a list -> 'a t
+end
+
+module ListSetExtendedImpl = struct
+  include ListSetImpl
+  let of_list lst = function 
+    | [] -> []
+    | h ::  t -> if mem h t then elements t else h :: elements t 
+end
+
+module ListSetExtended : SetExtended = ListSetExtendedImpl
+
+(* articulate difference between include and open*)
+module M = struct
+  let x = 0
+end
+
+module N = struct
+  include M
+  let y = x + 1
+end
+
+module O = struct
+  open M
+  let y = x + 1
+end
+
+(*
+include is more powerful than open. include means including every definition of module literally. the one that is included is involved with using module.
+open is not including a module. it just makes moudule as 0 in scope.    
+
+A metaphor for understanding this difference might be: open M imports definitions from M and makes them available for local consumption, but they aren’t exported to the outside world. Whereas include M imports definitions from M, makes them available for local consumption, and additionally exports them to the outside world.
+*)
